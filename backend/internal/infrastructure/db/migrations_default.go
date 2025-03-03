e4fbe8fc50ac8e4507af3453daca006a7801914c
Raúl Enrique González Bondarchuk <96430972+raulbondarchuk@users.noreply.github.com>
2025-03-03 21:18:11 +0100

    Default data

--- backend/internal/infrastructure/db/migrations_default.go
+++ backend/internal/infrastructure/db/migrations_default.go
@@ -15,6 +15,10 @@ func init_default_data(db *gorm.DB) error {
 		return err
 	}
 
+	if err := init_Roles(db); err != nil {
+		return err
+	}
+
 	if err := init_User(db); err != nil {
 		return err
 	}
@@ -22,35 +26,85 @@ func init_default_data(db *gorm.DB) error {
 	return nil
 }
 
+func init_Roles(db *gorm.DB) error {
+	var count int64
+	if err := db.Model(&models.RoleModel{}).Count(&count).Error; err != nil {
+		return err
+	}
+	if count == 0 {
+		if err := createRole(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.roles.company.id")),
+			viper.GetString("database.migrations.defaults.roles.company.name"),
+			viper.GetString("database.migrations.defaults.roles.company.desc"),
+		); err != nil {
+			return err
+		}
+
+		if err := createRole(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.roles.verificaciones.id")),
+			viper.GetString("database.migrations.defaults.roles.verificaciones.name"),
+			viper.GetString("database.migrations.defaults.roles.verificaciones.desc"),
+		); err != nil {
+			return err
+		}
+
+		if err := createRole(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.roles.secondary.id")),
+			viper.GetString("database.migrations.defaults.roles.secondary.name"),
+			viper.GetString("database.migrations.defaults.roles.secondary.desc"),
+		); err != nil {
+			return err
+		}
+
+		if err := createRole(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.roles.sat01.id")),
+			viper.GetString("database.migrations.defaults.roles.sat01.name"),
+			viper.GetString("database.migrations.defaults.roles.sat01.desc"),
+		); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // Initialize default provider entity
 func init_Provider(db *gorm.DB) error {
-	// Example: Check if there is at least one
 	var count int64
 	if err := db.Model(&models.ProviderModel{}).Count(&count).Error; err != nil {
 		return err
 	}
 	if count == 0 {
-		// Create default provider Liftel
-		defaultProviderLiftel := models.ProviderModel{
-			ID:   uint(viper.GetInt("database.migrations.defaults.provider.liftel.id")),
-			Name: viper.GetString("database.migrations.defaults.provider.liftel.name"),
-			Desc: viper.GetString("database.migrations.defaults.provider.liftel.desc"),
-		}
-		if err := db.Create(&defaultProviderLiftel).Error; err != nil {
+		if err := createProvider(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.provider.liftel.id")),
+			viper.GetString("database.migrations.defaults.provider.liftel.name"),
+			viper.GetString("database.migrations.defaults.provider.liftel.desc"),
+		); err != nil {
 			return err
 		}
-		log.Printf("Created default provider: %s with ID: %d", defaultProviderLiftel.Name, defaultProviderLiftel.ID)
 
-		// Create default provider Verificaciones
-		defaultProviderVerificaciones := models.ProviderModel{
-			ID:   uint(viper.GetInt("database.migrations.defaults.provider.verificaciones.id")),
-			Name: viper.GetString("database.migrations.defaults.provider.verificaciones.name"),
-			Desc: viper.GetString("database.migrations.defaults.provider.verificaciones.desc"),
-		}
-		if err := db.Create(&defaultProviderVerificaciones).Error; err != nil {
+		if err := createProvider(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.provider.verificaciones.id")),
+			viper.GetString("database.migrations.defaults.provider.verificaciones.name"),
+			viper.GetString("database.migrations.defaults.provider.verificaciones.desc"),
+		); err != nil {
 			return err
 		}
-		log.Printf("Created default provider: %s with ID: %d", defaultProviderVerificaciones.Name, defaultProviderVerificaciones.ID)
+
+		if err := createProvider(
+			db,
+			uint(viper.GetInt("database.migrations.defaults.provider.secondary.id")),
+			viper.GetString("database.migrations.defaults.provider.secondary.name"),
+			viper.GetString("database.migrations.defaults.provider.secondary.desc"),
+		); err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -74,6 +128,55 @@ func init_User(db *gorm.DB) error {
 			return err
 		}
 		log.Printf("Created default user: %s with ID: %d", defaultUser.Login, defaultUser.ID)
+
+		// Assign company role to the default user
+		if err := assignCompanyRoleToUser(db, defaultUser.ID); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func assignCompanyRoleToUser(db *gorm.DB, userID uint) error {
+	var companyRole models.RoleModel
+	if err := db.Where("role = ?", "company").First(&companyRole).Error; err != nil {
+		return err
+	}
+
+	userRole := models.RefRoleUserModel{
+		UserID: userID,
+		RoleID: companyRole.ID,
+	}
+	if err := db.Create(&userRole).Error; err != nil {
+		return err
+	}
+	log.Printf("Assigned company role to user ID: %d", userID)
+	return nil
+}
+
+func createProvider(db *gorm.DB, id uint, name, desc string) error {
+
+	provider := models.ProviderModel{
+		ID:   id,
+		Name: name,
+		Desc: desc,
+	}
+	if err := db.Create(&provider).Error; err != nil {
+		return err
+	}
+	log.Printf("Created default provider: %s with ID: %d", provider.Name, provider.ID)
+	return nil
+}
+
+func createRole(db *gorm.DB, id uint, name, desc string) error {
+	role := models.RoleModel{
+		ID:   id,
+		Role: name,
+		Desc: desc,
+	}
+	if err := db.Create(&role).Error; err != nil {
+		return err
+	}
+	log.Printf("Created default role: %s with ID: %d", role.Role, role.ID)
 	return nil
 }
